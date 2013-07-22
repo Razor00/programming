c31a8b62c53a32e23ad2191535aa5853e444b674
madan <madan.pandiri@emerson.com>
2013-07-22 11:46:04 +0530

    week2 assignment

--- algorithms/open-courses/stanford/algorithms/week2/qsort.py
+++ algorithms/open-courses/stanford/algorithms/week2/qsort.py
@@ -1,50 +1,63 @@
 #!/usr/bin/python
 
-
-def get_pivot1(st, end):
+import sys
+def get_pivot1(arr, st, end):
     return st
 
-def get_pivot2(st, end):
+def get_pivot2(arr, st, end):
     return end
 
+def get_largest(arr, a, b):
+    if arr[a] > arr[b]:
+        return a
+    return b
 
-def get_pivot3(st, end):
-    return 0
+def get_pivot3(arr, st, end):
+    mid = st + (end - st)/2
+
+    h = get_largest(arr, get_largest(arr, st, mid), end)
+
+    if h == end:
+        return get_largest(arr, st, mid)
+
+    if h == mid:
+        return get_largest(arr, st, end)
+
+    if h == st:
+        return get_largest(arr, mid, end)
 
 def print_data(arr, pivot):
-    print "%s, %s" % (st, end)
-    print pivot
+    return
+    print "pivot: ", pivot
     print arr
 
 def partition(arr, p, st, end):
     arr[p], arr[st] = arr[st], arr[p]
     arr_p = arr[st]
     i = j = st+1
-    print arr
-    while i <= j and j <= end:
-        while i <= end and arr[i] < arr_p:
-                i = i+1
-        while j <= end and arr[j] > arr_p:
-                j = j+1
-        if i < j and j <= end:
+
+    while j <= end:
+        if arr[j ] < arr_p:
             arr[i], arr[j] = arr[j], arr[i]
-#        print i, ",", j, "-", arr
-        i += 1
+            i += 1
         j += 1
-    i = i - 2
+    i = i - 1
     arr[st], arr[i] = arr[i], arr[st]
-    print_data(arr, p)
+    return i
 
 def qsort(array, st, end):
     if end <= st:
         return 0
-    print "%s, %s" % (st, end)
-    pivot = get_pivot1(st, end) 
-    partition(array, pivot, st, end)
-    sz = end - st + 1
+
+
+    pivot = get_pivot3(array, st, end)
+    pivot = partition(array, pivot, st, end)
     lt = qsort(array, st, pivot-1)
     rt = qsort(array, pivot+1, end)
-    return lt + rt + sz
+    cmp = end - st # lengof array = (end - st + 1) and subtract 1
+
+#   print "comparisons:", st, end, cmp
+    return lt + rt + cmp
 
 
 
@@ -56,6 +69,7 @@ N = int(f.readline().rstrip());
 array = [int(x.rstrip('\n\r')) for x in f]
 
 inversions = 0
-#partition(array, 4, 0, N-1)
-print qsort(array, 0, N-1)
+comp = qsort(array, 0, N-1)
+
+print comp
 
